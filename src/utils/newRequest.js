712bcddede38b33b43afaf55b40e16d5dae9bb39
sha1good <bankoleyakub11@yahoo.com>
2023-03-03 20:37:57 +0100

    cookies

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
- const newRequest = axios.create({
-    baseURL: "https://clone-fiverr-app-01.onrender.com/api",
-    withCredentials: true
- })
+const BASEURL = "https://clone-fiverr-app-01.onrender.com/api";
+const newRequest = axios.create({
+  baseURL: BASEURL,
+  withCredentials: true,
+  crossDomain: true,
+  headers: {
+    "Access-Control-Allow-Origin":
+      "https://clone-fiverr-client-app01.netlify.app",
+  },
+});
 
- export default newRequest;
+export default newRequest;
