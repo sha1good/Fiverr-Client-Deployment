574bd0bde4286eda2ad99620ec92a21fcaf2b6f5
sha1good <bankoleyakub11@yahoo.com>
2023-03-05 13:08:56 +0100

    removing dist build

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -4,7 +4,6 @@ const BASEURL = "https://clone-fiverr-app-01.onrender.com/api";
 const newRequest = axios.create({
   baseURL: BASEURL,
   withCredentials: true,
-  crossDomain: true,
  });
 
 export default newRequest;
